5de52e48191504e633195a39030dc8a9b1727485
zachha <zachha@gmail.com>
2019-01-26 17:44:46 -0500

    Update keyword validation and converts keyword into shift values

--- vigenere.c
+++ vigenere.c
@@ -5,57 +5,64 @@
 #include <stdlib.h>
 
 // declaring functions
-void validateKey(string key);
-int modKey(int rawKey);
-void getMessage(int validKey);
-void encryptMessage(int length, string message, int validKey);
+void validateKeyword(string keyword);
+string modKeyword(string rawKeyword);
+void getMessage(int validKeyword);
+void encryptMessage(int length, string message, int validKeyword);
 int convertLowercase(int encryptedLetter);
 int convertUppercase(int encryptedLetter);
 
 int main(int argc, string argv[])
 {
     if (argc != 2) {
-        printf("Usage: ./caesar key\n");
+        printf("Usage: ./vigenere keyword\n");
     } else {
-        validateKey(argv[1]);
-        int validKey = modKey(atoi(argv[1]));
-        getMessage(validKey);   
+        validateKeyword(argv[1]);
+        string validKeyword = modKeyword(argv[1]);
+        //getMessage(validKeyword);   
     }
 }
 
 // validates the key input by user by checking every char to make sure its a digit from 0-9 
-void validateKey(string key) {
-    int length = strlen(key);
+void validateKeyword(string keyword) {
+    int length = strlen(keyword);
         for (int i=0; i<length; i++) {
-            if (!isdigit(key[i])) {
-                printf("Usage: ./caesar key\n");
+            if (!isalpha(keyword[i])) {
+                printf("Usage: ./vigenere keyword\n");
                 exit(0);
-            } 
+            }
         }
 }
 
-// since there are 26 letters in the alphabet, if user inputs a key over 25 (26 would just give the same string back) we find the mod and make that remainder the new key. 
-int modKey(int rawKey) {
-    if(rawKey > 25) {
-        return rawKey %= 25;
-    } else {
-        return rawKey;
+// converts each char in the user's keyword to the value that it will shift the plaintext to encrypt it into ciphertext 
+string modKeyword(string rawKeyword) {
+    int rawLength = strlen(rawKeyword);
+    string validKeyword = rawKeyword;
+    for(int o=0; o<rawLength; o++) {
+        if(islower(validKeyword[o])) {
+            validKeyword[o] -= 97;
+            printf("Check: %i\n", validKeyword[o]);
+        } else {
+            validKeyword[o] -= 65;
+            printf("Check: %i\n", validKeyword[o]);
+        }
     }
+    return validKeyword;
 }
 
 // Asks the user for input then 'encrypts' the message by adding the key to the ASCII value and prints it back out to user as a string
-void getMessage(int validKey) {
+void getMessage(int validKeyword) {
     string message = get_string("plaintext: ");
     int messageLength = strlen(message);
-    encryptMessage(messageLength, message, validKey);
+    encryptMessage(messageLength, message, validKeyword);
 }
 
-void encryptMessage(int length, string message, int validKey) {
+void encryptMessage(int length, string message, int validKeyword) {
     for (int x=0; x<length; x++) {
         char letter = message[x];
         // checks each char to make sure it's alphabetic before converting it
         if(isalpha(letter)) {
-            int encryptedLetter = ((int) letter) + validKey;
+            int encryptedLetter = ((int) letter) + validKeyword;
             if(islower(letter)) {
                 message[x] = convertLowercase(encryptedLetter);
             } else {
@@ -67,11 +74,10 @@ void encryptMessage(int length, string message, int validKey) {
 }
 
 // ASCII:  lowercase a = 097 z = 122 uppercase A = 065 Z = 090
-// these two functions start from a in the alphabet if the ascii number goes past z
+// these two functions start over from 'a' in the alphabet if the ascii number goes past 'z'
 int convertLowercase(int encryptedLetter) {
     if(encryptedLetter > 122) {
         encryptedLetter = 96 + (encryptedLetter - 122);
-        //printf("checking: %i %c\n", encryptedLetter, encryptedLetter);
         return encryptedLetter;
     } else {
         return encryptedLetter;
@@ -81,7 +87,6 @@ int convertLowercase(int encryptedLetter) {
 int convertUppercase(int encryptedLetter) {
     if(encryptedLetter > 90) {
         encryptedLetter = 64 + (encryptedLetter - 90);
-        //printf("checking: %i %c\n", encryptedLetter, encryptedLetter);
         return encryptedLetter;
     } else {
         return encryptedLetter;
