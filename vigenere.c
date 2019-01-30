1cc6b83169e3d5f6a1c930055228d9c8b6c47b36
zachha <zachha@gmail.com>
2019-01-30 12:00:03 -0500

    Update vigenere.c with getShiftValues and relateShiftValues functions

--- vigenere.c
+++ vigenere.c
@@ -6,8 +6,9 @@
 
 // declaring functions
 void validateKeyword(string keyword);
-string modKeyword(string rawKeyword);
+string getShiftValues(string rawKeyword);
 void getMessage(int validKeyword);
+int relateShiftValues(string validKeyword);
 void encryptMessage(int length, string message, int validKeyword);
 int convertLowercase(int encryptedLetter);
 int convertUppercase(int encryptedLetter);
@@ -18,7 +19,7 @@ int main(int argc, string argv[])
         printf("Usage: ./vigenere keyword\n");
     } else {
         validateKeyword(argv[1]);
-        string validKeyword = modKeyword(argv[1]);
+        string validKeyword = getShiftValues(argv[1]);
         //getMessage(validKeyword);   
     }
 }
@@ -35,7 +36,7 @@ void validateKeyword(string keyword) {
 }
 
 // converts each char in the user's keyword to the value that it will shift the plaintext to encrypt it into ciphertext 
-string modKeyword(string rawKeyword) {
+string getShiftValues(string rawKeyword) {
     int rawLength = strlen(rawKeyword);
     string validKeyword = rawKeyword;
     for(int o=0; o<rawLength; o++) {
@@ -57,6 +58,18 @@ void getMessage(int validKeyword) {
     encryptMessage(messageLength, message, validKeyword);
 }
 
+// this should relate every char in the plain text to it's appropriate char in the keyword
+int relateShiftValues(string validKeyword) {
+    if (strlen(validKeyword) > 1) {
+        int keywordIndex = plaintext[i] % (strlen(validKeyword) - 1);
+        return keywordIndex;
+    } else {
+        int keywordIndex = 0;
+        return keywordIndex;
+    }
+}
+ 
+    
 void encryptMessage(int length, string message, int validKeyword) {
     for (int x=0; x<length; x++) {
         char letter = message[x];
