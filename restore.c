242f3bcb2786a3d9661a19d6adb98d47afa75154
zachha <zachha@gmail.com>
2019-02-16 23:08:00 -0500

    Update restore.c to take user argument and open image file.

--- restore.c
+++ restore.c
@@ -3,5 +3,18 @@
 
 int main(int argc, char *argv[])
 {
-
+    // ensure proper usage
+    if (argc != 2)
+    {
+        fprintf(stderr, "Usage: ./recover image\n");
+        return 1;
+    }
+    
+    // open input file
+    FILE *inptr = fopen(argv[1], "r");
+    if (inptr == NULL)
+    {
+        fprintf(stderr, "Could not open %s.\n", argv[1]);
+        return 2;
+    }
 }
