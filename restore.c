b54c3c4720c41325fdbc993fffa6e530e69eabf9
zachha <zachha@gmail.com>
2019-02-18 16:09:40 -0500

    Update restore.c with fully functional code to restore all JPEGS

--- restore.c
+++ restore.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 
 int main(int argc, char *argv[])
 {
@@ -17,11 +18,21 @@ int main(int argc, char *argv[])
         fprintf(stderr, "Could not open %s.\n", argv[1]);
         return 2;
     }
-    int buffer;
+    
+    typedef uint8_t  BYTE;
+
+    //card.raw is in 512 byte blocks.
+    BYTE *buffer = malloc(512 * sizeof(BYTE));
+    //buffer = malloc(512);
+
+    // declare our variables
+    int picFound = 0;
+    char filename[8];
     int picCount = 0;
+    FILE *img = NULL;
     
     // loops through the raw file to look for JPEGS and writes the JPEG files for those it finds
-    while (fread(&buffer, 512, 1, inptr) == 512) 
+    while (fread(buffer, 1, 512, inptr) == 512) 
     {
         // check the first 4 bytes of each 512 byte block for JPEG headers
         if (buffer[0] == 0xff &&
@@ -29,19 +40,39 @@ int main(int argc, char *argv[])
             buffer[2] == 0xff &&
             (buffer[3] & 0xf0) == 0xe0)
         {
-            // for creating outfiles for jpegs
+            // begins to write everything new files now that the first headers have been found
+            picFound = 1;
+            // closes image file if one is already opened
+            if (img != NULL) 
+            {
+                fclose(img);
+                picCount++;
+            }
+            // keeps track of filename for each image
             sprintf(filename, "%03i.jpg", picCount);
-            //char *outfile = filename;
-            FILE *img = fopen(filename, "w");
-            fwrite(&buffer, 512, 1, img);
-            fclose(img);
-            picCount++;
+            char *outfile = filename;
+            // for creating outfiles for jpegs
+            img = fopen(outfile, "w");
+            fwrite(buffer, 1, 512, img);
+            
         } 
+        else
+        {
+            // continues writing to JPEG until the next headers are found
+            if (picFound == 1) 
+            {
+                fwrite(buffer, 1, 512, img);
+            }
+        }
     } 
-    
     // if checkForEOF is not 512, that means we have reached the end of file so we should terminate the program
-    fclose(inputr);
+    fclose(inptr);
+    fclose(img);
+    // free up memory allocated to buffer
+    free(buffer);
+    
         
     // program success
     return 0;
 }
+
